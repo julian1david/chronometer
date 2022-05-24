35785eb149cc3947da751942edbe17a34192a560
JulianF <juliandavidbf@gmail.com>
2022-05-24 00:27:59 -0500

    @feat: update footer and colors

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# chronometer
-Cronometro realizando en el platziChallenge
+# Chronometer
+A simple example of a one Chronometer
+
+
+## Vista del proyecto 🚀
+
+Mira **[Chronometer-Page](https://julian1david.github.io/chronometer/)** para conocer como funciona el proyecto.
+
+
+### Instalación 🔧
+
+```
+git clone git@github.com:julian1david/chronometer.git
+```
+
+Inicia el ./public/index.html  tu navegador
+
+
+## Construido con 🛠️
+
+* SASS
+* HTML
+* CSS
+* JavaScript
+
+## Contribuyendo 🖇️
+
+Cualquier contirbución al proyecto es bien recibida ! 
+
+## Autor ✒️
+
+* **Julian F**  - [julian1f](https://github.com/julian1david)
+
+
+## Licencia 📄
+
+Este proyecto está bajo la Licencia (MIT) - mira el archivo [LICENSE](LICENSE) para detalles
+
+---
+Hecho con ❤️ por [julian1f](https://github.com/julian1david) 😊
--- src/index.js
+++ src/index.js
@@ -1,34 +1,49 @@
 let seconds = document.querySelector('#seconds');
 let minutes = document.querySelector('#minutes');
 let hours = document.querySelector('#hours');
+let animable = document.querySelectorAll('.Time__side-a');
+let animations = [...animable];
 let secondsValue = 0;
 let minutesValue = 0;
 let hoursValue = 0;
 let currentTimer;
 
+animations.forEach(item => item.addEventListener('animationend', () => {
+    item.classList.remove('Toggled')
+}));
 
+const cronometro = () => {
+    secondsValue++;
+    seconds.textContent = ("0" + secondsValue).slice(-2);
+    animable[2].classList.add('Toggled');
+    //Cuando el intervalo lleve secondsValue a 59            
+    if (secondsValue == 4) {
+        secondsValue = -1;
+    }
+    if (secondsValue == 0) {
+        minutesValue++;
+        minutes.textContent = ("0" + minutesValue).slice(-2);
+        animations[1].classList.add('Toggled');
+    }
+    if (minutesValue == 2) {
+        minutesValue = -1;
+    }
+    if (secondsValue == 0 && minutesValue == 0) {
+        hoursValue++;
+        hours.textContent = ("0" + hoursValue).slice(-2);;
+        animations[0].classList.add('Toggled');
+
+    }
+}
 const startTimer = () => {
     currentTimer = setInterval(() => {
-        secondsValue ++;
-        seconds.textContent = ('0' + secondsValue).slice(-2);
-        if (secondsValue >= 4){
-            minutesValue++;
-            secondsValue = 0;
-            minutes.textContent = ('0' + minutesValue).slice(-2);
-            if(minutesValue >= 2){
-                minutesValue = 0;
-                minutes.textContent = ('0' + minutesValue).slice(-2);
-                hoursValue++
-                hours.textContent = ('0' + hoursValue).slice(-2);
-            }
-        }
-    },1000)
+        cronometro()
+    }, 1000)
 }
 
 const pauseTimer = () => {
     clearInterval(currentTimer);
-    console.log("Pause");
-} 
+}
 
 const resetTimer = () => {
     minutesValue = 0;
@@ -37,26 +52,29 @@ const resetTimer = () => {
     seconds.textContent = '00'
     minutes.textContent = '00'
     hours.textContent = '00'
+    clearInterval(currentTimer)
 }
 
 
 let buttons = document.querySelector('#buttons');
-let play =  document.querySelector('#start');
+let play = document.querySelector('#start');
+let pause = document.querySelector('#pause');
 
 buttons.addEventListener('click', (e) => {
-    if(e.target.id === "start"){
+    if (e.target.id === "start") {
         startTimer();
-        e.target.setAttribute(`disabled`,``);
+        e.target.setAttribute(`disabled`, ``);
+        pause.removeAttribute(`disabled`);
     }
-    else if (e.target.id === "pause"){
+    else if (e.target.id === "pause") {
         play.removeAttribute(`disabled`)
+        e.target.setAttribute(`disabled`, ``);
         pauseTimer()
     }
-    else if (e.target.id === "reset"){
+    else if (e.target.id === "reset") {
+        pause.removeAttribute(`disabled`);
         play.removeAttribute(`disabled`)
         resetTimer();
     }
 })
 
-document.getElementsByClassName
-document.getElementsByTagName
