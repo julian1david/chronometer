90bb87438411c3d0134f43aae2e8e4d9cee7bfe5
JulianF <juliandavidbf@gmail.com>
2022-05-27 16:24:01 -0500

    @feat: add timer and pomodoro styl

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ let seconds = document.querySelector('#seconds');
 let minutes = document.querySelector('#minutes');
 let hours = document.querySelector('#hours');
 let animable = document.querySelectorAll('.Time__side-a');
+const heroContainer = document.querySelector('.Hero__time');
+let play = document.querySelector('#start');
+let pause = document.querySelector('#pause');
+let title = document.querySelector('.Hero__title')
+
 let animations = [...animable];
 let secondsValue = 0;
 let minutesValue = 0;
@@ -12,11 +17,131 @@ animations.forEach(item => item.addEventListener('animationend', () => {
     item.classList.remove('Toggled')
 }));
 
-function formatTime(valor){
+function formatTime(valor) {
     return ("0" + valor).slice(-2)
 }
 
+const divContainer = ({
+    id: idUser,
+    title: paragraph,
+
+}) => {
+    const listContainer = []
+    const containerTime = document.createElement('div');
+
+    const container = document.createElement('div');
+    container.className = 'Time'
+    const sideA = document.createElement('span');
+    sideA.className = 'Time__side Time__side-a'
+    const textView = document.createElement('p');
+    textView.setAttribute('id', `${idUser}`);
+    textView.className = 'Time__text';
+    textView.textContent = '00'
+    const sideB = document.createElement('span');
+    sideB.className = 'Time__side Time__side-n'
+
+    container.append(sideA, textView, sideB)
+    const title = document.createElement('p');
+    title.textContent = paragraph;
+    title.className = 'Time__description'
+
+    listContainer.push(container, title);
+
+    containerTime.append(...listContainer);
+
+
+    return containerTime;
+}
+
+
+//Funcion para pintar chronometros
+
+const painClock = () => {
+
+    heroContainer.innerHTML = ""
+    const timers = [];
+
+    const hoursContainer = divContainer({
+        id: "hours",
+        title: "Hours"
+    });
+    const minutesContainer = divContainer({
+        id: "minutes",
+        title: "Minutes"
+    });
+    const secondsContainer = divContainer({
+        id: "seconds",
+        title: "Seconds"
+    });
+
+    timers.push(hoursContainer, minutesContainer, secondsContainer);
+    heroContainer.append(...timers)
+
+    console.log(heroContainer);
+
+    seconds = document.querySelector('#seconds');
+    minutes = document.querySelector('#minutes');
+    hours = document.querySelector('#hours');
+
+    return heroContainer
+}
+
+
+// paint Timer 
+
+
+const divInputs = () => {
+    
+    const inputsTimers = [];
+    
+    const inputContainer =  document.createElement('div');
+    inputContainer.className  = 'Input'
+    const inputHours = document.createElement('input');
+    inputHours.className = 'Input__item'
+    const inputMinutes = document.createElement('input');
+    inputMinutes.className = 'Input__item'
+    const inputSeconds = document.createElement('input');
+    inputSeconds.className = 'Input__item';
+
+    inputHours.setAttribute('id', 'inputMinutes');
+    inputMinutes.setAttribute('id','inputMinutes');
+    inputSeconds.setAttribute('id','inputseconds');
+
+    inputHours.setAttribute('type','text');
+    inputMinutes.setAttribute('type','text');
+    inputSeconds.setAttribute('type','text');
+
+    inputHours.setAttribute('placeholder','Hours');
+    inputMinutes.setAttribute('placeholder','Minutes');
+    inputSeconds.setAttribute('placeholder','Seconds');
+
+    inputsTimers.push(inputHours,inputMinutes,inputSeconds);
+
+    inputContainer.append(...inputsTimers);
+
+    return inputContainer
+    
+
+}
+const paintTimer = ()  => {
+
+    title.textContent = 'Timer';
+    const inputsDivs = divInputs();
+    heroContainer.append(inputsDivs);
+    console.log(inputsDivs);
+    if(play.id === "start"){
+        play.id = "startTimer";
+    }
+
+}
+
+
+const removeinput = () => {
+    const inputContainer = document.querySelector('.Input');
+    heroContainer.removeChild(inputContainer)
+}
 const cronometro = () => {
+
     secondsValue++;
     seconds.textContent = formatTime(secondsValue);
     animable[2].classList.add('Toggled');
@@ -59,14 +184,18 @@ const resetTimer = () => {
     clearInterval(currentTimer)
 }
 
+const timer = () => {
 
-let play = document.querySelector('#start');
-let pause = document.querySelector('#pause');
+}
+
+
+
 
 document.addEventListener('click', (e) => {
-    const clickedElement =  e.target
+    const clickedElement = e.target
+
+    //Chronomer
     if (clickedElement.matches('#start')) {
-        console.log(clickedElement);
         startTimer();
         play.classList.add(`not-active`);
         pause.classList.remove(`not-active`);
@@ -80,6 +209,16 @@ document.addEventListener('click', (e) => {
         play.classList.remove(`not-active`);
         pause.classList.remove(`not-active`);
         resetTimer();
+    }
+
+
+    //Timer
+    else if (clickedElement.matches('#timer')){
+        paintTimer();
+    }
+    else if (clickedElement.matches('#startTimer')){
+        removeinput();
+        console.log('hello');
     }
 })
 
